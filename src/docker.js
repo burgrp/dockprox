8cbed948b6f43fe938990b6eafb884e0978bc396
Pavel Burgr <pb@drake.cz>
2019-09-03 01:12:13 +0200

    Initial commit

--- src/docker.js
+++ src/docker.js
@@ -1,9 +1,46 @@
+const { Docker } = require("node-docker-api");
+
 module.exports = async config => {
+
+    const docker = new Docker({ socketPath: config.socket });
+    const domain = config.domain;
+
     return {
         async createMapping() {
-            return {
-                x: 1
-            }
+            let containers = await Promise.all((await docker.container.list()).map(c => c.status()));            
+            
+            return containers.map(c => {
+                
+                let hostname = c.data.Config.Labels["cz.drake.dockprox.host"] || c.data.Config.Hostname;
+                
+                let containerName = c.data.Name;
+                if (containerName.startsWith("/")) {
+                    containerName = containerName.substring(1);
+                }
+            
+                if (hostname.length === 12 && hostname.match(/^[0-9a-f]+$/)) {
+                    
+                    hostname = containerName;
+                    
+                    let composeMatch = /^.+_(?<name>.*)_[0-9]+$/.exec(hostname);
+                    if (composeMatch) {
+                        hostname = composeMatch.groups.name;
+                    }
+                }
+
+                if (hostname.indexOf(".") === -1) {
+                    hostname = hostname + "." + (c.data.Config.Domainname || domain);
+                }
+
+                return {
+                    front: {                        
+                        host: hostname
+                    },
+                    back: {
+                        host: containerName
+                    }
+                }
+            });
         }
     }
 }
